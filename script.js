ea8205abb68b38984dc83a72d621f82e27503a19
rettdev <lucasmara904@gmail.com>
2023-06-14 12:35:21 -0300

    changes

--- script.js
+++ script.js
@@ -17,26 +17,45 @@ function OnclickBtnSearch() {
     const thumbnailLink = document.getElementById('thumbnail-link');
     const thumbnailImage = document.getElementById('thumbnail-image');
     const divResult = document.getElementById('result');
+    const footer = document.getElementById('container-footer');
 
-    if (getLink.value.trim() === "") {
+    const link = getLink.value.trim();
+    const isShortsLink = link.includes("youtube.com/shorts");
+
+    if (link === ""){
         errorMessage.textContent = "Escreva um link válido no campo abaixo!";
         errorMessage.style.display = "block";
         return;
     }
-    else {
+
+    if (isShortsLink){
+        errorMessage.textContent = "Desculpe, links do YouTube Shorts não são suportados.";
+        errorMessage.style.display = "block";
+        return;
+    }
+
+    if(errorMessage.style.display !== "none"){
         errorMessage.style.display = "none";
+    }
+
+    if(footer.style.position !== "fixed"){
+        footer.style.position = "fixed";
     }
 
     videoTitle.textContent = ""; // Limpar o título anterior, se houver
     thumbnailLink.href = ""; // Defina a URL correta para o link de download
     thumbnailImage.src = `https://img.youtube.com/vi/${extractVideoId(getLink.value)}/maxresdefault.jpg`;
 
-    const apiKey = 'AIzaSyAQzLR53MRdrAHsbrLe7IL5jvVYm8TAR1A';
+    const apiKey = 'AIzaSyBuVmeIjXAdKMb71cdL4ZHGIa43EhyijtI';
     const videoId = extractVideoId(getLink.value);
     getVideoDetails(videoId, apiKey).then(snippet => {
         videoTitle.textContent = snippet.title;
         thumbnailImage.src = snippet.thumbnails.maxres.url;
-        getElementById.style.display = "block";
+        divResult.style.display = "block";
+        errorMessage.textContent = "Sua thumbanil está disponível, role para baixo e clique na imagem!";
+        errorMessage.style.color = "green";
+        errorMessage.style.display = "block";
+        footer.style.position = "static";
     }).catch(error => {
         errorMessage.textContent = "Erro ao obter detalhes do vídeo.";
         errorMessage.style.display = "block";
